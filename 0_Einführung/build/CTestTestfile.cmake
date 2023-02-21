2bc28909f740482f5c233f02163c56c1bee0d677
Robin F. Roth <100561465+RobinfRoth@users.noreply.github.com>
2023-02-22 00:01:51 +0100

    Delete 0_Einführung/build directory

--- 0_Einführung/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robin/git/prcpp/0_Einführung
-# Build directory: /home/robin/git/prcpp/0_Einführung/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
